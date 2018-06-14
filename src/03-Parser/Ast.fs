f0d1dce7f739cb03114c86ec63c3f043e9dd7d7d
Anton Tcholakov <anton.pt@outlook.com>
2018-06-14 22:08:47 +0100

    First parser implementation (pending conflict resolution)

---
+++ src/03-Parser/Ast.fs
@@ -0,0 +1,64 @@
+﻿module Ast
+
+type Id = string
+type TypeId = string
+
+type ArithmeticOp =
+    | PlusOp
+    | MinusOp
+    | TimesOp
+    | DivOp
+
+type ComparisonOp =
+    | EqualsOp
+    | NotEqualsOp
+    | GreaterThanOp
+    | LessThanOp
+    | GreaterThanOrEqualsOp
+    | LessThanOrEqualsOp
+
+type LogicOp =
+    | AndOp
+    | OrOp
+
+type TypeDef =
+    | TypeAliasDef of TypeId
+    | RecordTypeDef of (Id * TypeId) list
+    | ArrayTypeDef of TypeId
+    
+type LValue =
+    | LId of Id
+    | LProperty of LValue * Id
+    | LArrayElem of LValue * Expr
+and Expr =
+    | NilExpr
+    | UnitExpr
+    | EvalExpr of LValue
+    | ExprSeqExpr of Expr list
+    | IntLiteralExpr of int
+    | StringLiteralExpr of string
+    | FunctionCallExpr of Id * Expr list
+    | NegationExpr of Expr
+    | ArithmeticExpr of Expr * ArithmeticOp * Expr
+    | ComparisonExpr of Expr * ComparisonOp * Expr
+    | LogicExpr of Expr * LogicOp * Expr
+    | RecordCreationExpr of TypeId * (Id * Expr) list
+    | ArrayCreationExpr of TypeId * Expr * Expr
+    | AssignmentExpr of LValue * Expr
+    | IfThenExpr of Expr * Expr
+    | IfThenElseExpr of Expr * Expr * Expr
+    | WhileDoExpr of Expr * Expr
+    | ForDoExpr of Id * Expr * Expr * Expr
+    | BreakExpr
+    | LetInExpr of Dec list * Expr
+and Dec =
+    | TDec of TypeDec
+    | VDec of VarDec
+    | FDec of FunDec
+and TypeDec = TypeDec of TypeId * TypeDef
+and VarDec = 
+    | VarDec of Id * Expr
+    | VarDecWithType of Id * TypeId * Expr
+and FunDec =
+    | FunDec of Id * (Id * TypeId) list * Expr
+    | FunDecWithType of Id * (Id * TypeId) list * TypeId * Expr
---
+++ src/03-Parser/Demo.fsx
@@ -0,0 +1,10 @@
+﻿#r "../../packages/FsLexYacc.Runtime.7.0.6/lib/portable-net45+netcore45+wpa81+wp8+MonoAndroid10+MonoTouch10/FsLexYacc.Runtime.dll"
+#r "bin/Debug/03-Parser.dll"
+
+open Parser
+open Microsoft.FSharp.Text.Lexing
+
+let prog = """(a := 5; a)"""
+
+let buf = LexBuffer<_>.FromString prog
+let ast = start Lexer.token buf
