64faf0c2aa54d9ca54fb27e1c82bd0a54763c7e0
Anton Tcholakov <anton.pt@outlook.com>
2018-07-29 16:56:44 +0100

    Copy implementation of AST into new Semantics project

---
+++ src/05-Semantics/Ast.fs
@@ -0,0 +1,51 @@
+﻿module Ast
+
+open Symbol
+open Microsoft.FSharp.Text.Lexing
+
+type Var =
+    | SimpleVar of Symbol * Position
+    | FieldVar of Var * Symbol * Position
+    | SubscriptVar of Var * Exp * Position
+and Exp =
+    | VarExp of Var
+    | NilExp
+    | IntExp of int
+    | StringExp of string * Position
+    | CallExp of func: Symbol * args: Exp list * pos: Position
+    | OpExp of left: Exp * oper: Oper * right: Exp * pos: Position
+    | RecordExp of fields: (Symbol * Exp * Position) list * typ: Symbol * pos: Position
+    | SeqExp of (Exp * Position) list
+    | AssignExp of var: Var * exp: Exp * pos: Position
+    | IfExp of testExp: Exp * thenExp: Exp * elseExp: Exp option * pos: Position
+    | WhileExp of testExp: Exp * body: Exp * pos: Position
+    | ForExp of var: Symbol * escape: bool ref * lo: Exp * hi: Exp * body: Exp * pos: Position
+    | BreakExp of Position
+    | LetExp of decs: Dec list * body: Exp * pos: Position
+    | ArrayExp of typ: Symbol * size: Exp * init: Exp * pos: Position
+and Dec =
+    | FunDec of FunDec list
+    | VarDec of name: Symbol * escape: bool ref * typ: (Symbol * Position) option * init: Exp * pos: Position
+    | TypeDec of TypeDec list
+and Field = 
+    { FieldName: Symbol 
+      Escape: bool ref
+      Typ: Symbol
+      Pos: Position }
+and FunDec =
+    { FunName: Symbol
+      Params: Field list 
+      Result: (Symbol * Position) option
+      Body: Exp
+      Pos: Position }
+and TypeDec =
+    { TypeName: Symbol
+      Ty: Ty
+      Pos: Position }
+and Ty =
+    | NameTy of Symbol * Position
+    | RecordTy of Field list
+    | ArrayTy of Symbol * Position
+and Oper =
+    | PlusOp | MinusOp | TimesOp | DivideOp
+    | EqOp | NeqOp | LtOp | LeOp | GtOp | GeOp
---
+++ src/05-Semantics/Symbol.fs
@@ -0,0 +1,24 @@
+﻿module Symbol
+
+open System.Collections.Generic
+
+type Symbol = private Symbol of string * int
+
+let private nextSym = ref 0
+let private symbolDictionary = Dictionary<string, int>()
+
+let symbol name =
+    if symbolDictionary.ContainsKey name then
+        Symbol (name, symbolDictionary.[name])
+    else
+        symbolDictionary.[name] <- !nextSym
+        nextSym := !nextSym + 1
+        Symbol (name, symbolDictionary.[name])
+
+let name (Symbol (n, _)) = n
+
+type Table<'a> = private Table of Map<Symbol, 'a>
+
+let empty = Table (Map.empty)
+let enter (Table tbl) s x = Table (tbl |> Map.add s x)
+let look (Table tbl) s = tbl |> Map.tryFind s
